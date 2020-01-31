733dd43620984a67d1d562297d66b6e7dc4d3eaa
Finem50 <Finem50@live.com>
2020-01-30 23:20:05 -0600

    Using digit-wide formatting on string output. Also jumping the gun a bit by using makefiles but it's just going to make life easier from here-on-out.

--- ch1/s1-2/temp_conv.c
+++ ch1/s1-2/temp_conv.c
@@ -15,7 +15,8 @@ int main(){
 	while(fahr <= upper){
 
 		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+//		printf("%d\t%d\n", fahr, celsius);
+		printf("%3d %6d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 
